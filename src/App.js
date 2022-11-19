bcb8c4920295e4eddf2495cc21c710d9f6dd097b
sebaw65 <sebaw65@gmail.com>
2022-11-19 17:39:25 +0100

    dodanie komponentu validate

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 
 import EquationResult from "./components/UI/EquationResult";
 import InputForm from "./components/UI/InputForm";
+import Validate from "./calculate/Validate";
 
 function App() {
   const [result, setResult] = useState("");
-
   const getValueFromField = (el) => {
     // console.log(el.target.value);
     setResult(el.target.value);
@@ -15,12 +15,12 @@ function App() {
 
   return (
     <div className="App">
-      <EquationResult displayResult={result} />
       <InputForm
         valueHandler={
           getValueFromField
         } /* przekazanie parametru którym posłużę się do wyciągnięcia wartości inputa */
       />
+      <Validate enteredString={result} />
       {/* <div>Znak</div>
       <div>Znak</div>
       <div>Znak</div>
--- src/components/UI/EquationResult.js
+++ src/components/UI/EquationResult.js
@@ -3,21 +3,7 @@ import React from "react";
 // import styles from "./EquationResult.module.css";
 
 const EquationResult = (props) => {
-  let result;
-  console.log(equation(props.displayResult));
-
-  // Zablokować obliczanie w przypadku wpisania innego znaku niż cyfry. Należy rozpoznać znak i wykonać odpowienie działanie po wpisaniu po znaku cyfry.
-  function equation(input) {
-    return new Function("return " + input)();
-  }
-
-  if (props.displayResult == undefined) {
-    if (!isNaN(props.displayResult))
-      return (result = "Wprowadź prawidłowe równanie");
-    else result = equation(props.displayResult);
-  }
-
-  return <div>Wynik to: {result}</div>;
+  return <div>Wynik to: {props.displayResult}</div>;
 };
 
 export default EquationResult;
